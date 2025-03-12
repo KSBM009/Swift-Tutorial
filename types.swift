7411ad1fa0b57c73c5cde48ffeeb0767e64769c3
ksbm009 <kevinstephenbiju2003@gmail.com>
2025-03-12 10:11:04 +0530

    Going through initial variable declarations.

---
+++ types.swift
@@ -0,0 +1,15 @@
+// Int
+var highScore: Int = 0
+var currScore = 0
+
+// Float and Double
+// Float has a precision of 6 decimal places
+// Double has a precision of 15 decimal places
+var percentComplete: Double = 0.76
+
+// String
+var userThought: String = "I hope this course is good."
+var myName: String = "Kevin"
+
+// Bool
+var isDarkModeOn: Bool = true
