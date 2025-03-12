60b14bcbd3011a4615dbcace180d92947210b685
ksbm009 <kevinstephenbiju2003@gmail.com>
2025-03-12 10:31:03 +0530

    Variables and Constants

---
+++ varsNconsts.swift
@@ -0,0 +1,10 @@
+// Var is used to define a variable
+var curScore = 0
+
+// let is used to define a Constant
+let highScore = 0
+let myName = "Kevin"
+
+// Constants can not be changed.
+// highScore = 55
+curScore = 55
