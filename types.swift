6e53f1c39fc582e722901aa8338e79e195f834f9
ksbm009 <kevinstephenbiju2003@gmail.com>
2025-03-12 10:11:52 +0530

    Contd. to the previous commit.

--- types.swift
+++ types.swift
@@ -10,6 +10,7 @@ var percentComplete: Double = 0.76
 // String
 var userThought: String = "I hope this course is good."
 var myName: String = "Kevin"
+var myName = "Kevin Stephen Biju"
 
 // Bool
 var isDarkModeOn: Bool = true
