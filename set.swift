700968fff721ef2385f3eaba3c56a3fd20efe476
ksbm009 <kevinstephenbiju2003@gmail.com>
2025-03-13 09:51:52 +0530

    Sets in Swift

---
+++ set.swift
@@ -0,0 +1,20 @@
+// An Array
+var ages = [18, 33, 55, 17, 94, 26, 33, 17]
+
+// An Empty Set Declaration
+// var ageSet: Set<Int> = []
+
+// Creating a set from an Array
+var ageSet = Set(ages)
+
+print(ageSet)
+
+// Checking for elements
+ageSet.contains(17)
+ageSet.comtains(101)
+
+// Inserting new Elements
+ageSet.insert(101)
+
+ageSet.comtains(101)
+print(ageSet)
