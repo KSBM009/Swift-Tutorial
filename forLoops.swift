a2a61a8bfd62a3c0144b6e5506e60a7e0daa8da5
ksbm009 <kevinstephenbiju2003@gmail.com>
2025-03-13 14:22:42 +0530

    For Loops in Swift

---
+++ forLoops.swift
@@ -0,0 +1,12 @@
+// All Stars Team
+let allStars = ["James", "Davis", "Harden", "Doncic", "Leonard"]
+
+// For Loop 
+for player in allStars {
+    print(player)
+}
+
+// For Loop with where clause
+for player in allStars where player == "Davis" {
+    print("Found Davis from the Array!")
+}
