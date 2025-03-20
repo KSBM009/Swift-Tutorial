5a03d0bb9157faa7334cec0732c0d4f1b769db33
ksbm009 <kevinstephenbiju2003@gmail.com>
2025-03-20 14:27:03 +0530

    Class in Swift

---
+++ class.swift
@@ -0,0 +1,34 @@
+class Developer {
+    // ? is used to handle NIL
+    var name: String?
+    var jobTitle: String?
+    var yrsExp: Int?
+
+    // Empty Initializer
+    init() {}
+    
+    init(name: String, jobTitle: String, yrsExp: Int) {
+        self.name = name
+        self.jobTitle = jobTitle
+        self.yrsExp = yrsExp
+    }
+
+    func speakName() {
+        print(name!)
+    }
+
+}
+
+let kevin = Developer(name: Kevin, jobTitle: Jr. iOS Developer, yrsExp: 1)
+
+print(kevin.name)
+print(kevin.jobTitle)
+print(kevin.yrsExp)
+
+kevin.speakName()
+
+let arif = Developer()
+
+print(arif.name)
+print(arif.jobTitle)
+print(arif.yrsExp)
