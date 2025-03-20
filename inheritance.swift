3b867c0e85802467c66063bfa8c3af00bfbea14f
ksbm009 <kevinstephenbiju2003@gmail.com>
2025-03-20 17:14:46 +0530

    Inheritance in Swift.

---
+++ inheritance.swift
@@ -0,0 +1,49 @@
+// Inheritance is an OOPs concept.
+// Inheriting Buttons Example available at https://youtu.be/CwA1VWP0Ldw?t=8616&si=osVRvkN7l80DYmuZ
+
+class Developer {
+    // ? is used to handle NIL
+    var name: String?
+    var jobTitle: String?
+    var yrsExp: Int?
+
+    // Empty Initializer
+    init() {}
+    
+    init(name: String, jobTitle: String, yrsExp: Int) {
+        self.name = name
+        self.jobTitle = jobTitle
+        self.yrsExp = yrsExp
+    }
+
+    func speakName() {
+        print(name!)
+    }
+
+}
+
+class iOSdeveloper: Developer {
+    var favFramework: String?
+
+    func speakFavFramework() {
+        if let favFramework = favFramework {
+            print(favFramework)
+        } else {
+            print(self.name + " don't have a favourite framework.")
+        }
+    }
+
+    override func speakName() {
+        print("\(name!) - \(jobTitle!)")
+    }
+}
+
+let kevin = iOSdeveloper(name: Kevin, jobTitle: Developer, yrsExp: 3)
+
+kevin.speakFavFramework()
+
+kevin.favFramework = "UI Kit"
+
+kevin.speakFavFramework()
+
+kevin.speakName()
